72a05a3a121cad34b19c60a62e435bbbb746599d
timlenny <schlotter.tim@gmail.com>
2023-01-26 15:25:41 +0100

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int coffee = 3;
+
+
+        while(coffee > 0){
+            System.out.println("Code :)");
+            coffee--;
+        }
+
+        System.out.println("Sleep :(");
+
+    }
+}
